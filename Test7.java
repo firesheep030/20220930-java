66011bfab23a14850b5f96e7093cbfa0f2445e65
firesheep030 <82532192+firesheep030@users.noreply.github.com>
2022-10-07 09:49:23 +0800

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Test7{
+    public static void main(String[] args){
+        int grade=0;
+        
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        grade = input.nextInt();
+        if(grade>100){
+			System.out.println("bad");
+				
+		}
+			else{
+				switch (grade / 10){
+					case 9:
+					case 10:
+						System.out.println("A");
+						break;
+			
+					case 8:
+						System.out.println("B");
+						break;
+			
+					case 7:
+						System.out.println("C");
+						break;
+			
+					case 6:
+						System.out.println("D");
+						break;
+			
+					default:
+						System.out.println("E");
+						break;
+				}
+			}
+        
+	}
+}
