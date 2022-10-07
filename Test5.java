fcba338670db538ac2fa0978154d0b83401e8394
firesheep030 <82532192+firesheep030@users.noreply.github.com>
2022-10-07 09:31:09 +0800

    20221007

---
+++ Test5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Test5{
+	public static void main(String[] args){
+		int grade;
+        
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter first integer:");
+		grade = input.nextInt();
+		
+		if (grade >= 90 & grade<=100) {
+			System.out.println("A");
+		}
+		else if (grade >= 80 & grade<=89){
+			System.out.println("B");
+		}
+		else if (grade >= 70 & grade<=79){
+			System.out.println("C");
+		}
+		else if (grade >= 60 & grade<=69){
+			System.out.println("D");
+		}
+		else{
+			System.out.println("E");
+		}
+	}
+}
+
